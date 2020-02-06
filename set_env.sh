dad9af56fe11d11a807480f9f282977201221c47
Jaebak Kim <jbkim@ucsb.edu>
2020-02-05 18:15:11 -0800

    Run scons with multiple cores

--- set_env.sh
+++ set_env.sh
@@ -4,7 +4,6 @@
 
 RUN_KERNEL=$(uname -r | cut -d '-' -f1)
 
-
 if [ "$RUN_KERNEL" == "3.10.0" ]; then
   export SCRAM_ARCH=slc7_amd64_gcc700
   cd /net/cms29/cms29r0/pico/cc7/CMSSW_10_2_11_patch1/src
@@ -14,6 +13,8 @@ fi
 
 eval `scramv1 runtime -sh`
 cd -
+
+export SCONSFLAGS="-j $(nproc --all)"
 
 source $(dirname $(readlink -e "$BASH_SOURCE"))/modules/jb_utils/set_env.sh
 source $(dirname $(readlink -e "$BASH_SOURCE"))/modules/queue_system/set_env.sh
