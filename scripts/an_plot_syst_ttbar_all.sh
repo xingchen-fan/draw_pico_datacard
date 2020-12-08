9acc701dda54b9adfc6cb5016af521ddddb740d6
Jaebak Kim <jbkim@ucsb.edu>
2020-12-08 08:30:28 -0800

    Modified weights

--- scripts/an_plot_syst_ttbar_all.sh
+++ scripts/an_plot_syst_ttbar_all.sh
@@ -9,11 +9,28 @@ mkdir -p plots.an/ttbar/2018
 rm -f plots.an/ttbar/2018/*.pdf
 mkdir -p plots.an/ttbar/run2
 rm -f plots.an/ttbar/run2/*.pdf
-scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2016 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2016
 mv plots/*.pdf plots.an/ttbar/2016
-scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2017 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2017
 mv plots/*.pdf plots.an/ttbar/2017
-scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2018 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2018
 mv plots/*.pdf plots.an/ttbar/2018
-scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2016,2017,2018 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2016,2017,2018
 mv plots/*.pdf plots.an/ttbar/run2
+
+mkdir -p plots.an/ttbar.unblind/2016
+rm -f plots.an/ttbar.unblind/2016/*.pdf
+mkdir -p plots.an/ttbar.unblind/2017
+rm -f plots.an/ttbar.unblind/2017/*.pdf
+mkdir -p plots.an/ttbar.unblind/2018
+rm -f plots.an/ttbar.unblind/2018/*.pdf
+mkdir -p plots.an/ttbar.unblind/run2
+rm -f plots.an/ttbar.unblind/run2/*.pdf
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2016 --unblind
+mv plots/*.pdf plots.an/ttbar.unblind/2016
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2017 --unblind
+mv plots/*.pdf plots.an/ttbar.unblind/2017
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2018 --unblind
+mv plots/*.pdf plots.an/ttbar.unblind/2018
+scons && ./run/higgsino/an_plot_syst_ttbar.exe --year 2016,2017,2018 --unblind
+mv plots/*.pdf plots.an/ttbar.unblind/run2
--- scripts/plot_n_minus_1_all.sh
+++ scripts/plot_n_minus_1_all.sh
@@ -9,23 +9,48 @@ mkdir -p plots.an/n_minus_1/2018
 rm -f plots.an/n_minus_1/2018/*.pdf
 mkdir -p plots.an/n_minus_1/run2
 rm -f plots.an/n_minus_1/run2/*.pdf
-scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year 2016 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2016 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2016 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2016 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year 2016 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2016 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2016 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2016 
 mv plots/*.pdf plots.an/n_minus_1/2016
-scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year 2017 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2017 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2017 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2017 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year 2017 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2017 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2017 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2017 
 mv plots/*.pdf plots.an/n_minus_1/2017
-scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year 2018 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2018 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2018 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2018 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year 2018 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2018 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2018 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2018 
 mv plots/*.pdf plots.an/n_minus_1/2018
-scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year run2 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year run2 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year run2 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
-scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year run2 2>&1 | tee trash_log && cat trash_log | grep open | awk -F ' ' '{print $2}' | xargs -tI {} imgcat {}
+scons && ./run/higgsino/plot_n_minus_1.exe --sample search --year run2
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year run2
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year run2
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year run2
 mv plots/*.pdf plots.an/n_minus_1/run2
+
+mkdir -p plots.an/n_minus_1.unblind/2016
+rm -f plots.an/n_minus_1.unblind/2016/*.pdf
+mkdir -p plots.an/n_minus_1.unblind/2017
+rm -f plots.an/n_minus_1.unblind/2017/*.pdf
+mkdir -p plots.an/n_minus_1.unblind/2018
+rm -f plots.an/n_minus_1.unblind/2018/*.pdf
+mkdir -p plots.an/n_minus_1.unblind/run2
+rm -f plots.an/n_minus_1.unblind/run2/*.pdf
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2016 --unblind
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2016 --unblind
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2016 --unblind
+mv plots/*.pdf plots.an/n_minus_1.unblind/2016
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2017 --unblind 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2017 --unblind 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2017 --unblind 
+mv plots/*.pdf plots.an/n_minus_1.unblind/2017
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year 2018 --unblind 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year 2018 --unblind 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year 2018 --unblind 
+mv plots/*.pdf plots.an/n_minus_1.unblind/2018
+scons && ./run/higgsino/plot_n_minus_1.exe --sample ttbar  --year run2 --unblind 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample zll    --year run2 --unblind 
+scons && ./run/higgsino/plot_n_minus_1.exe --sample qcd    --year run2 --unblind 
+mv plots/*.pdf plots.an/n_minus_1.unblind/run2
