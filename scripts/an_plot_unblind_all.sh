d0fefa9719e0642cfa5fe258b0adc47e4aed4950
Jaebak Kim <jbkim@ucsb.edu>
2021-04-12 09:15:47 -0700

    Made some more options for plots in plot_kappas.cxx and an_plot_syst_ttbar.cxx Added a script that can generate plots for the app_unblinding.tex

---
+++ scripts/an_plot_unblind_all.sh
@@ -0,0 +1,14 @@
+mkdir -p plots.trash
+mv plots/*.pdf plots.trash
+mkdir -p tables.trash
+mv tables/* tables.trash
+mkdir -p plots.an/unblind
+set -e // Exit when any command fails
+
+scons&& run/higgsino/plot_kappas.exe --sample search --year run2 --scen mc --string_options search_ttbar_same_bins
+scons&& run/higgsino/plot_kappas.exe --sample ttbar --unblind --year run2 --scen data --string_options search_ttbar_same_bins
+scons&& run/higgsino/an_plot_syst_ttbar.exe --year run2 --unblind --string_options plot_planes,plot_btags
+scons&& run/higgsino/table_sample_regions.exe --year run2 --no_signal --string_options split_mc_in_detai
+
+mv plots/* plots.an/unblind
+mv tables/table_search_regions.pdf plots.an/unblind
