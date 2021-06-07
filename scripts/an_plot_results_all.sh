c9010c7ee5c65e19251f50219f95cd6b198455ff
Jaebak Kim <jbkim@ucsb.edu>
2021-06-06 18:51:11 -0700

    Added script that makes table with zbi significances using kappa code

--- scripts/an_plot_results_all.sh
+++ scripts/an_plot_results_all.sh
@@ -6,6 +6,6 @@ mkdir -p plots.an/results
 set -e // Exit when any command fails
 rp run/higgsino/plot_n_minus_1.exe --sample search --year run2 --unblind --unblind_signalregion
 rp run/higgsino/plot_search_unblind.exe --year run2 --unblind --unblind_signalregion
-rp run/higgsino/plot_kappas.exe --sample search --unblind --year run2 --scen data
+rp run/higgsino/plot_kappas.exe --sample search --unblind --year run2 --scen data --string_option do_zbi
 mv plots/* plots.an/results
 mv tables/*.tex plots.an/results
