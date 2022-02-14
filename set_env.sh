dfd263d67fdae0a0b7f4177d3e9749b3ab2bb3dd
Jaebak Kim <jbkim@cmslpc149.fnal.gov>
2022-02-14 14:32:52 -0600

    Modified set_env to get rid of errors related with setting up ROOT.

--- set_env.sh
+++ set_env.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-. /cvmfs/cms.cern.ch/cmsset_default.sh
-
 RUN_KERNEL=$(uname -r | cut -d '-' -f1)
 
-if [ "$RUN_KERNEL" == "3.10.0" ]; then
-  export SCRAM_ARCH=slc7_amd64_gcc700
-  cd /net/cms29/cms29r0/pico/cc7/CMSSW_10_2_11_patch1/src
-elif [ "$RUN_KERNEL" == "2.6.32" ]; then
-  cd /net/cms29/cms29r0/pico/CMSSW_10_2_11_patch1/src
+# Setting up root using cmssw at ucsb servers. 
+# Please setup ROOT yourself if you are not working on a ucsb server.
+if [[ $HOSTNAME =~ .*physics.ucsb.edu ]]; then
+
+  . /cvmfs/cms.cern.ch/cmsset_default.sh
+
+  if [ "$RUN_KERNEL" == "3.10.0" ]; then
+    export SCRAM_ARCH=slc7_amd64_gcc700
+    cd /net/cms29/cms29r0/pico/cc7/CMSSW_10_2_11_patch1/src
+  elif [ "$RUN_KERNEL" == "2.6.32" ]; then
+    cd /net/cms29/cms29r0/pico/CMSSW_10_2_11_patch1/src
+  fi
+  
+  . /cvmfs/cms.cern.ch/cmsset_default.sh
+  eval `scramv1 runtime -sh`
+  cd -
 fi
 
-. /cvmfs/cms.cern.ch/cmsset_default.sh
-eval `scramv1 runtime -sh`
-cd -
+if [ -z ${ROOTSYS} ]; then
+  echo "Please setup ROOT"
+fi
 
 export SCONSFLAGS="-j $(nproc --all)"
 
